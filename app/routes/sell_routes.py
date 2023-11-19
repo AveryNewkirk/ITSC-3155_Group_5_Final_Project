d1c5299f6470b90bfdca81a020d95ed38d9e7bc2
Riley_Retzloff <rirye811@gmail.com>
2023-11-19 01:54:41 -0500

    Cleaned up some more

--- app/routes/sell_routes.py
+++ app/routes/sell_routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Blueprint, render_template, request, redirect, url_for
 
 sell = Blueprint('sell', __name__)
@@ -5,4 +6,3 @@ sell = Blueprint('sell', __name__)
 @sell.route('/sell', methods=["GET", "POST"])
 def create_listing():
     return render_template('sell.html')
-
