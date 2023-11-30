ab1c72afa89bd16fc580abd88b671ae8d0022b49
Mekhi <mekhi.j.glover@gmail.com>
2023-11-30 14:07:32 -0500

    login and sign up functions work

--- app/routes/signup.py
+++ app/routes/signup.py
@@ -4,6 +4,7 @@ from ..database import db
 from app.models.pipeline import Users
 #from __init__ import bcrypt
 from flask_bcrypt import Bcrypt
+import bcrypt
 signup = Blueprint('signup', __name__)
 
 @signup.route('/signup')
@@ -11,19 +12,18 @@ def display():
     return render_template('signup.html')
 
 
-@signup.route('/')
+@signup.route('/home')
 def home():
     return render_template('profile.html')
 
 @signup.post('/create')
 def create():
-    username = request.form.get('username')
     email = request.form.get('email')
     password = request.form.get('password')
-    if not username or not password or not email:
+    if not password or not email:
         abort(400)
-    hashed_password = Bcrypt.generate_password_hash(password, 12)
-    new_user = Users(username,email,hashed_password)
+    #hashed_password = Bcrypt.generate_password_hash(password,12).decode() # type: ignore
+    new_user = Users(email,password)
     db.session.add(new_user)
     db.session.commit()
-    return redirect('/')
+    return redirect('/home')
