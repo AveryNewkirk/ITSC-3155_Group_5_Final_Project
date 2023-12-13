8a3d3b0a446887af467b42e3b3e31dbaae46cf8a
Mekhi <mekhi.j.glover@gmail.com>
2023-12-13 10:31:58 -0500

    Some changes

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,6 +1,8 @@
+from typing import Self
 from flask import Blueprint, render_template, session,redirect
-from ..models.pipeline import Users
+from src.models.pipeline import Users
 user = Blueprint('user', __name__)
+
 
 @user.route('/user')
 def user_page():
@@ -9,11 +11,11 @@ def user_page():
 #endpoint for singe usser page, redirects to profile if they are in the session.
 @user.route('/user/<string:username>' , methods = ['POST','GET'])
 def user_singleton(username):
-    
+    temp_user = Users.query.filter_by(username = username).first()
     if username in session.values() and session['username'] == username:
         return redirect('/profile')
     
-    usr = Users.get_by_username(username=username)
+    usr = Users.get_by_username(username = username)
 
     if usr.get_access() == False:
         return redirect('/')
