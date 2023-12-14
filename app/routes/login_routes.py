6a6b13cec370f3620548afd4805d7c1572c0d98b
Mekhi <mekhi.j.glover@gmail.com>
2023-12-14 16:15:02 -0500

    moved from src to app

---
+++ app/routes/login_routes.py
@@ -0,0 +1,46 @@
+from flask import Blueprint, render_template, request, redirect, url_for, abort, session
+from src.database import bcrypt
+
+from src.models.pipeline import Users
+login = Blueprint('login', __name__)
+
+
+@login.route('/')
+def home():
+    return render_template('/index.html')
+
+
+@login.route('/login')
+def display():
+    if 'user' in session:
+        return redirect('/secret')
+    return render_template('login.html')
+
+@login.post('/auth')
+def authUser():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not password or not email:
+        abort(400)
+    temp = Users.query.filter_by(email = email).first_or_404()
+    if bcrypt.check_password_hash(password,temp.password):
+        session['email'] = temp.email
+        session['username'] = temp.username
+        return redirect('/secret')
+    else:
+        abort(401)
+
+@login.route('/secret')
+def get_user_page():
+    if 'user' not in session:
+        abort(401)
+    return render_template('profile.html', username = session['user'].get('username'))
+
+
+@login.post('/logout')
+def signout():
+    del session['user']
+    return redirect('/')
+
+
---
+++ app/routes/user_routes.py
@@ -0,0 +1,37 @@
+#from crypt import methods
+from flask import Blueprint, render_template, session,redirect
+from src.models.pipeline import Users, Listing
+user = Blueprint('user', __name__)
+
+
+@user.route('/user')
+def user_page():
+    return render_template('user.html')
+
+#endpoint for singe usser page, redirects to profile if they are in the session.
+@user.route('/user/<username>' , methods = ['POST','GET'])
+def user_singleton(username):
+
+    if username in session.values() and session['username'] == username:
+        return redirect('/profile')
+    
+    #usr = Users.get_by_username(username = username)
+    userNow = Users.query.filter_by(username = username).first
+
+    if userNow.get_access() == False:
+        return redirect('/')
+    
+    return render_template('user.html',username=username)
+
+@user.route('/user/<int:user_id>', methods=['GET', 'POST'])
+def user_singleton_id(user_id):
+    user = Users.query.get(user_id)
+    username = None
+    if isinstance(user, Users):
+        username = user.username
+    else:
+        return redirect('/')
+    if username in session.values() and session['username'] == username:
+        return redirect('/profile')
+    return render_template('user.html', username=username)
+    
