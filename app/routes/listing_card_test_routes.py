1740685c8454afcbd244b275b5d3d9fe7bde18b7
Riley_Retzloff <rirye811@gmail.com>
2023-12-14 17:06:42 -0500

    Implemented update and delete functionality for listings.

--- app/routes/listing_card_test_routes.py
+++ app/routes/listing_card_test_routes.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Blueprint, render_template
 from ..models.pipeline import Listing, Album, Photo
 
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Blueprint, render_template, session,redirect
 from ..models.pipeline import Users
 user = Blueprint('user', __name__)
